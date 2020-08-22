b1d890f2f9a1b320a09c35a68ea1ee8899b56a00
Kelly <kap09c@gmail.com>
2020-08-21 18:08:08 -0700

    beta version, remaining work is likely minor tweaks and styling

--- Apps/Faradays_Law/Faradays_Law.js
+++ Apps/Faradays_Law/Faradays_Law.js
@@ -11,6 +11,32 @@ function checkandrun(){
   }
 }
 
+function fieldshape(x,y,s){
+
+  
+ 
+  if(s<0){
+    ctx.beginPath()
+    ctx.moveTo(x+s, y+s)
+    ctx.lineTo(x-s, y-s)
+    ctx.moveTo(x+s, y+s)
+    ctx.moveTo(x+s, y-s)
+    ctx.lineTo(x-s, y+s)
+    ctx.stroke()
+  }else if(s>0){
+    ctx.beginPath()
+    ctx.arc(x,y,s,0, Math.PI *2, false)
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.arc(x,y,s/5,0, Math.PI *2, false)
+    ctx.fill()
+    
+
+  }
+  
+
+}
+
  
 //Loop data
 var index=[], As=[1,1,1,1], dA=[], zeroes=[]
@@ -44,6 +70,7 @@ var c_h=canvas.height
 var c_c=[c_w*0.5,c_h*0.5]
 
 ctx.font = '24px sans-serif';
+ctx.textAlign = 'center';
 
 
 //////////////////Default Loop////////////////////////////////////////////////
@@ -65,9 +92,13 @@ function labeled_point(ctx,x_p,y_p,x_l,y_l,pointsize, l_text){
 ///////////////////////////Draw Loop////////////////////////////////////////////////////
 
 function draw() {
-  ctx.textAlign = 'left';
+  
+  //Update values and check to see if the loop should run
+  
+  var l_now=Math.sqrt(A_now)*100
 
-  A_in=Math.min(100,Math.max(0, -0.5*c_w+l_x+100))*A_now/100
+  A_in=Math.min( l_now,Math.max(0, -0.5*c_w+l_x+ l_now))* l_now/10000
+
   As.shift()
   As.push(A_in)
 
@@ -77,8 +108,8 @@ function draw() {
   dA.shift()
   dA.push(0.1*(As[0]*Bf[0] +As[1]*Bf[1]-As[2]*Bf[2]-As[3]*Bf[3])/(0.01) )
  
-  update=false
-
+  
+ //Test for nonzero values
   for(i=0;i<dA.length;i++){
     if(dA[i]!=zeroes[i]){
       update=true
@@ -86,106 +117,109 @@ function draw() {
     }
   }
 
-  if(update){
+  //if nonzero data, redraw
+  if(update){ 
 
-  //Clear draw area
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.clearRect(0, 0, c_w, c_h); // clear canvas
-  
-
-
-  
-  ctx.fillStyle='rgba(147, 189, 147,1)'
-  ctx.beginPath();
-  ctx.rect(0.5*c_w, 0,0.5*c_w,c_h, false);
-  ctx.fill();
-  
- 
-  ctx.fillStyle='rgb(113, 142, 97)'
-
-  for(i=0;i<10;i++){
-
-    for(j=0;j<5;j++){
-      ctx.beginPath();
-      labeled_point(ctx,0.5*c_w*(1+j/5)+10 ,c_h*(i/10)+30 ,5,5,0, 'X'  )
+    //Clear draw area
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.clearRect(0, 0, c_w, c_h); // clear canvas
     
-    }
-
-  }
- 
 
 
-  ctx.fillStyle='rgba(255, 210, 132,1)'
-  ctx.strokeStyle='black'
-  ctx.lineWidth= 5
-  ctx.beginPath();
-  ctx.rect(l_x, l_y,100,100, false);
-  ctx.stroke();
-  ctx.globalCompositeOperation = 'exclusion';
-  ctx.fill()
-
-
-  ctx.globalCompositeOperation = 'source-over';
-
-  ctx.fillStyle='rgba(181,181,181,1)'
-  ctx.beginPath();
-  ctx.arc(l_x, l_y+50,20,0, 2*Math.PI, false);
-  ctx.stroke();
-  ctx.fill()
-
-
-
-  ctx.fillStyle='black'
-  ctx.textAlign = 'center';
-  ctx.beginPath()
-  labeled_point(ctx, l_x-5, l_y+55 ,5,5,0, 'V'  )
-  ctx.font = '18px sans-serif';
-
+    //Magnetic Field Area
+    ctx.fillStyle='rgba(147, 189, 147,1)'
+    ctx.beginPath();
+    ctx.rect(0.5*c_w, 0,0.5*c_w,c_h, false);
+    ctx.fill();
+    
+    //Magnetic Field Line Indicators
+    ctx.fillStyle='rgb(113, 142, 97)'
+    ctx.strokeStyle='rgb(113, 142, 97)'
+    ctx.lineWidth= 2
   
-  ctx.beginPath()
-  labeled_point(ctx, l_x+55, l_y+20 ,5,5,0, '\u03D5 = '  )
-  labeled_point(ctx, l_x+55, l_y+50 ,5,5,0,  (Bf[3]*A_in).toFixed(2) )
-  labeled_point(ctx, l_x+55, l_y+80 ,5,5,0,   ' T m\u00B2'  )
- 
- 
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.font = '24px sans-serif';
-
- 
-  Plotly.animate('graph', 
-  {
-    data: [trace_v],},
-  {
-    transition: {
-      duration: 0,
-    },
-    frame: {
-      duration: 0,
-      redraw: false
+    for(i=0;i<10;i++){ 
+      for(j=0;j<10;j++){
+        fieldshape(0.5*c_w*(1+j/10)+15 ,c_h*(i/10)+15,10*B_now)
+      }
     }
-  })
-
-  ctx.textAlign = 'center';
-
-  labeled_point(ctx, 0.25*c_w, c_h-30 ,5,5,0, 'B = 0'  )
-  labeled_point(ctx, 0.75*c_w, c_h-30 ,5,5,0, 'B = '+ B_now.toFixed(1) +'T'  )
-
-  window.requestAnimationFrame(draw);
-}else{
   
-  running=false
+
+    //Draw the coil
+    ctx.fillStyle='rgba(255, 210, 132,1)'
+    ctx.strokeStyle='black'
+    ctx.lineWidth= 5
+    ctx.beginPath();
+    ctx.rect(l_x, l_y, l_now, l_now, false);
+    ctx.stroke();
+
+    //Highlight area overlapping with the field by exclusion overlay
+    ctx.globalCompositeOperation = 'exclusion';
+    ctx.fill()
+
+    //Return to regular drawing
+    ctx.globalCompositeOperation = 'source-over';
+
+    //Draw Voltmeter
+    ctx.fillStyle='rgba(181,181,181,1)'
+    ctx.beginPath();
+    ctx.arc(l_x, l_y+50,20,0, 2*Math.PI, false);
+    ctx.stroke();
+    ctx.fill()
+
+
+    //Label the Voltmeter
+    ctx.fillStyle='black'
+    ctx.beginPath()
+    labeled_point(ctx, l_x-5, l_y+55 ,5,5,0, 'V'  )
+    
+
+    //Display the current flux through the coil
+    ctx.font = '18px sans-serif';
+    ctx.beginPath()
+    labeled_point(ctx, l_x+l_now*0.5, l_y +l_now*(0.5-0.3) ,5,5,0, '\u03D5 = '  )
+    labeled_point(ctx, l_x+l_now*0.5, l_y +l_now*(0.5-0.0) ,5,5,0,  (Bf[3]*A_in).toFixed(2) )
+    labeled_point(ctx, l_x+l_now*0.5, l_y +l_now*(0.3+0.5) ,5,5,0,   ' T m\u00B2'  )
+  
+    //Identify sides of the image
+    ctx.font = '24px sans-serif';
+    labeled_point(ctx, 0.25*c_w, c_h-30 ,5,5,0, 'B = 0'  )
+    labeled_point(ctx, 0.75*c_w, c_h-30 ,5,5,0, 'B = '+ B_now.toFixed(1) +'T'  )
+
+    //Update plotly data
+    Plotly.animate('graph', 
+    {
+      data: [trace_v],},
+    {
+      transition: {
+        duration: 0,
+      },
+      frame: {
+        duration: 0,
+        redraw: false
+      }
+    })
+
+    
+
+    
+
+    update=false // Don't update again unless there is new data
+    window.requestAnimationFrame(draw);
+}else{ // otherwise conserve resources, don't redraw
+    running=false//allow draw to be triggered again
 }
   
 
 }
 
-//Canvas Mouse Events
+
+/////////Canvas Mouse and Touch Events///////////////////////////////////////////////////
 
 canvas.addEventListener('mousedown', function (ev){
   
   mouseisdown=true
   console.log(mouseisdown)
-  l_x=ev.pageX - rect.left-50
+  l_x=ev.pageX - rect.left-0
   l_y=ev.pageY - rect.top-50
   
   
@@ -196,40 +230,37 @@ canvas.addEventListener('mouseup', function (ev){
 
 })
 canvas.addEventListener('mousemove', function (ev){
-
   if (mouseisdown){
-  l_x=ev.pageX - rect.left-50
+  l_x=ev.pageX - rect.left-0
   l_y=ev.pageY - rect.top-50
   checkandrun()
- 
   }
- 
-
 })
 
 canvas.addEventListener('touchstart', function (ev){
   mouseisdown=true
-  
 })
 canvas.addEventListener('touchend', function (ev){
   mouseisdown=false
-
-
 })
-canvas.addEventListener('touchmove', function (ev){
 
+canvas.addEventListener('touchmove', function (ev){
   if (mouseisdown){
   l_x=ev.touches[0].clientX - canvas.offsetLeft-50
   l_y=ev.touches[0].clientY - canvas.offsetTop-50
   checkandrun()
   }
-
 })
 
 
-init();
 
 
+
+
+
+
+
+/////////Plotly Stuff///////////////////////////////////////////////////////////////
 
 ////////Trace
 trace_v={
@@ -239,9 +270,6 @@ trace_v={
   line: {shape: 'spline'},
   'smoothing': 1.3
 }
-
-
-
 
 var layout = {
   showgrid: false,
@@ -280,16 +308,18 @@ var layout = {
     margin: {
       l: 0,
       r: 0,
-      b: 50,
+      b: 70,
       t: 10,
       pad: 4
     },
     paper_bgcolor: 'rgba(255,255,255,0)',
     plot_bgcolor: 'rgba(255,255,255,0)'
   };
+
+
   Plotly.newPlot('graph', [trace_v],layout,{displayModeBar: false, scrollZoom: true})
 
-
+/////////Sliders/////////////////////////////////////////////////////////////////////
   b_slider=document.getElementById('bf_sl')
   b_out=document.getElementById('bf_o')
   b_out.innerHTML=b_slider.value/10
@@ -298,6 +328,7 @@ var layout = {
     
     b_out.innerHTML=b_slider.value/10
     B_now=b_slider.value/10
+    update=true
     checkandrun()
 
   }
@@ -309,6 +340,12 @@ var layout = {
 
     a_out.innerHTML=a_slider.value/10
     A_now=a_slider.value/10
+    update=true
     checkandrun()
 
   }
+
+
+
+
+init();
